a9d850263deb81537589d259aee2236d7a1f2bd4
VoHoangTuan <vohoangtuan@VoHoangTuans-iMac.local>
2018-05-06 21:03:37 +0700

    Initial commit with pod lib template

---
+++ Example/Pods/Target Support Files/MyLibraryIOS/MyLibraryIOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyLibraryIOS : NSObject
+@end
+@implementation PodsDummy_MyLibraryIOS
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyLibraryIOS_Example/Pods-MyLibraryIOS_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyLibraryIOS_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MyLibraryIOS_Example
+@end
